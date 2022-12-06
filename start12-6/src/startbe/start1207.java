fd962e962601bccb3869c63aeb95b6edbd9638ba
82105 <82105@ramscom>
2022-12-07 04:19:37 +0900

    최초 깃 연동

---
+++ start12-6/src/startbe/start1207.java
@@ -0,0 +1,16 @@
+package startbe;
+
+import java.util.Scanner;
+
+public class start1207 {
+
+	public static void main (String[] args) {
+		
+		int a = 3;
+		int b = 4;
+	    
+ 
+		
+		System.out.println((a + b)+ "=" + "?");
+	}
+} 
